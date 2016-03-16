8e80a8602392fdba361cb01807fc36910f0e160d
Craig M. Stimmel <cstimmel@uw.edu>
2016-03-16 14:59:04 -0700

    Refactor into a function main() can call.

--- slacker.py
+++ slacker.py
@@ -5,16 +5,21 @@ import urllib3.contrib.pyopenssl
 urllib3.contrib.pyopenssl.inject_into_urllib3()
 
 
-def main():
-    token = settings.SLACK_API_TOKEN
+def hello(channel, greeting):
     client = SlackClient(token)
 
+    print client.api_call("im.open", user="U0S04BRMJ")
+    print client.api_call("chat.postMessage",
+                          as_user="false",
+                          channel=channel,
+                          text=greeting)
+
+def main(token):
     channel = "#bot-testing"
     greeting = "Hello world!"
-
-    print client.api_call("im.open", user="U0S04BRMJ")
-    print client.api_call("chat.postMessage", as_user="false", channel=channel, text=greeting)
+    hello(channel, greeting)
 
 
 if __name__ == "__main__":
-    main()
+    token = settings.SLACK_API_TOKEN
+    main(token)
